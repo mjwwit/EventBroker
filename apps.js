344a0a6d26afa5d6102e9719900156524751c9f1
Michael de Wit <michael@Michaels-MacBook-Pro.local>
2015-04-17 13:29:19 +0200

    Now switching between a local and a global event broker.

--- apps.js
+++ apps.js
@@ -1,28 +1,35 @@
 (function (){
     'use strict';
 
-    angular.module('app1', []).controller('ctrl1', ['$scope', function ($scope) {
-        var myBroker = new EventBroker('app1');
+    angular.module('app1', ['brokr'])
+        .service('AppBroker', ['EventBrokerFactory', function (EventBroker) {
+            return new EventBroker('app1');
+        }])
+        .controller('ctrl1', ['$scope', 'AppBroker', function ($scope, AppBroker) {
+            AppBroker.on('app2', 'SOME_EVENT', alert);
 
-        myBroker.on('app2', 'SOME_EVENT', alert);
+            $scope.trigger = function () {
+                AppBroker.emitAsync('SOME_EVENT', 'Hello 1!').then(function () {
+                    console.log('Async promise resolved!');
+                });
+                console.log('Should come first!');
+            }
+        }]);
 
-        $scope.trigger = function () {
-            myBroker.emitAsync('SOME_EVENT', 'Hello 1!').then(function () {
-                console.log('Async promise resolved!');
-            });
-            console.log('Should come first!');
-        }
-    }]);
+    angular.module('app2', ['brokr'])
+        .config(['EventBrokerFactoryProvider', function (EventBrokerFactoryProvider) {
+            EventBrokerFactoryProvider.useGlobalBroker(false);
+        }])
+        .service('AppBroker', ['EventBrokerFactory', function (EventBroker) {
+            return new EventBroker('app2');
+        }])
+        .controller('ctrl2', ['$scope', 'AppBroker', function ($scope, AppBroker) {
+            AppBroker.on('app1', 'SOME_EVENT', alert);
 
-    angular.module('app2', []).controller('ctrl2', ['$scope', function ($scope) {
-        var myBroker = new EventBroker('app2');
-
-        myBroker.on('app1', 'SOME_EVENT', alert);
-
-        $scope.trigger = function () {
-            myBroker.emit('SOME_EVENT', 'Hello 2!');
-        }
-    }]);
-
-    angular.bootstrap(document.getElementById("app2"),['app2']);
+            $scope.trigger = function () {
+                AppBroker.emit('SOME_EVENT', 'Hello 2!');
+            }
+        }]);
 })();
+
+angular.bootstrap(document.getElementById("app2"),['app2']);
--- broker.js
+++ broker.js
@@ -1,37 +1,10 @@
-(function (){
+(function () {
     'use strict';
 
-    window.$$listeners = {};
-    window.$$eventBroker = {
-        $on: function (source, event, listener) {
-            var e = [source, event].join('_');
-            if ($$listeners[e] === undefined) {
-                $$listeners[e] = [];
-            }
-            $$listeners[e].push(listener);
-        },
-        $emit: function (source, event, data) {
-            $$listeners[[source, event].join('_')].forEach(function (listener) {
-                listener(data);
-            });
-        },
-        $emitAsync: function (source, event, data) {
-            var promises = [];
-            $$listeners[[source, event].join('_')].forEach(function (listener) {
-                promises.push(Q.async(function () {
-                    try {
-                        listener(data);
-                    } catch (e) {
-                        throw e;
-                    }
-                }));
-            });
-
-            return Q.all(promises.map(function (async) { async(); }));
+    function EventBroker(moduleName, $$eventBroker) {
+        if ($$eventBroker === undefined) {
+            $$eventBroker = __eventBroker;
         }
-    };
-
-    window.EventBroker = function (moduleName) {
         return {
             on: function (source, event, listener) {
                 $$eventBroker.$on(source, event, listener);
@@ -43,5 +16,110 @@
                 return $$eventBroker.$emitAsync(moduleName, event, data);
             }
         };
-    };
+    }
+
+    angular.module('brokr', [])
+        .provider('EventBrokerFactory', function EventBrokerFactoryProvider() {
+            this.useGlobalBroker = function(value) {
+                window.__useGlobalBroker = !!value;
+            };
+
+            this.$get = ['$$eventBroker', function EventBrokerFactoryBuilder($$eventBroker, moduleName) {
+                if (window.__useGlobalBroker) {
+                    defineGlobalEventBroker();
+                    return function (moduleName) {
+                        return new EventBroker(moduleName);
+                    };
+                }
+
+                return function (moduleName) {
+                    return new EventBroker(moduleName, $$eventBroker);
+                };
+            }];
+        })
+        .service('$$eventBroker', ['$q', function ($q) {
+            var $$listeners = {};
+            return {
+                $on: function (source, event, listener) {
+                    var e = [source, event].join('_');
+                    if ($$listeners[e] === undefined) {
+                        $$listeners[e] = [];
+                    }
+                    $$listeners[e].push(listener);
+                },
+                $emit: function (source, event, data) {
+                    var src = [source, event].join('_');
+                    if (!$$listeners[src]) {
+                        return;
+                    }
+                    $$listeners[src].forEach(function (listener) {
+                        listener(data);
+                    });
+                },
+                $emitAsync: function (source, event, data) {
+                    var src = [source, event].join('_');
+                    var promises = [];
+                    if ($$listeners[src]) {
+                        if ($$listeners) {
+                            $$listeners[src].forEach(function (listener) {
+                                promises.push($q(function (resolve, reject) {
+                                    try {
+                                        listener(data);
+                                        resolve();
+                                    } catch (e) {
+                                        reject(e);
+                                    }
+                                }));
+                            });
+                        }
+                    }
+
+                    return $q.all(promises);
+                }
+            };
+        }])
+    ;
+
+    function defineGlobalEventBroker(){
+        if (!window.__listeners) {
+            window.__listeners = {};
+        }
+        if (!window.__eventBroker) {
+            window.__eventBroker = {
+                $on: function (source, event, listener) {
+                    var e = [source, event].join('_');
+                    if (__listeners[e] === undefined) {
+                        __listeners[e] = [];
+                    }
+                    __listeners[e].push(listener);
+                },
+                $emit: function (source, event, data) {
+                    var src = [source, event].join('_');
+                    if (!__listeners[src]) {
+                        return;
+                    }
+                    __listeners[src].forEach(function (listener) {
+                        listener(data);
+                    });
+                },
+                $emitAsync: function (source, event, data) {
+                    var promises = [];
+                    var src = [source, event].join('_');
+                    if (__listeners[src]) {
+                        __listeners[[source, event].join('_')].forEach(function (listener) {
+                            promises.push(Q.async(function () {
+                                try {
+                                    listener(data);
+                                } catch (e) {
+                                    throw e;
+                                }
+                            }));
+                        });
+                    }
+
+                    return Q.all(promises.map(function (async) { async(); }));
+                }
+            };
+        }
+    }
 })();
